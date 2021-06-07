4cfd57f891a1ff63c52072747c896ca32923da9f
Krzysztof Sobolewski <krzysztof.sobolewski@starburstdata.com>
2021-06-07 16:33:28 +0200

    Add @CanIgnoreReturnValue to some methods in MatchAssert
    This is an AssertJ annotation, but IDEA knows about it (and ErrorProne
    probably too). It prevents a warning in the IDE that the return value is
    ignored, which is a false positive in this case, because we don't have
    to string more assertions. This makes the use optional without
    suppressing any more warnings inside the method.

--- core/trino-main/src/test/java/io/trino/operator/window/matcher/MatchAssert.java
+++ core/trino-main/src/test/java/io/trino/operator/window/matcher/MatchAssert.java
@@ -23,6 +23,7 @@ import io.trino.sql.planner.rowpattern.ir.IrLabel;
 import io.trino.sql.planner.rowpattern.ir.IrRowPattern;
 import org.assertj.core.api.AbstractAssert;
 import org.assertj.core.api.AssertProvider;
+import org.assertj.core.util.CanIgnoreReturnValue;
 
 import java.util.Map;
 
@@ -53,6 +54,7 @@ public class MatchAssert
         return () -> new MatchAssert(matcher.run(identityEvaluator(mappedInput)), labelMapping);
     }
 
+    @CanIgnoreReturnValue
     public MatchAssert hasLabels(char[] expectedLabels)
     {
         int[] mappedExpected = new int[expectedLabels.length];
@@ -65,6 +67,7 @@ public class MatchAssert
                         .isEqualTo(mappedExpected));
     }
 
+    @CanIgnoreReturnValue
     public MatchAssert hasCaptures(int[] expectedCaptures)
     {
         return satisfies(actual -> assertThat(actual.isMatched()).isTrue())
@@ -73,6 +76,7 @@ public class MatchAssert
                         .isEqualTo(expectedCaptures));
     }
 
+    @CanIgnoreReturnValue
     public MatchAssert isNoMatch()
     {
         return satisfies(actual -> assertThat(actual.isMatched()).isFalse());
